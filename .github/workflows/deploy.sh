82f5edb14f907ec843baf269c1e8c3a52813ffc8
xieyuanbin <xieyuanbin@acoinfo.com>
2023-06-21 16:55:34 +0800

    aaa

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
+pwd
 cd public
+pwd
 
 msg='来自github action的自动部署'
 githubUrl=https://xybin1990:${GITHUB_TOKEN}@github.com/xybin1990/xybin1990.github.io.git
@@ -12,4 +14,6 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
+pwd
 rm -rf public
+ls -al
