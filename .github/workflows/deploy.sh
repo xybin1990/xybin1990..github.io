31e653a19e11843a00966a26f850ee94a95d0416
xieyuanbin <xieyuanbin@acoinfo.com>
2023-06-21 17:02:54 +0800

    add log

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-echo "pwd"
+echo "[pwd]"
 pwd
 cd public
-echo "pwd"
+echo "[pwd]"
 pwd
 
 msg='来自github action的自动部署'
 githubUrl=https://xybin1990:${GITHUB_TOKEN}@github.com/xybin1990/xybin1990.github.io.git
 
 git init
+echo "[branch]"
 git branch
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
-echo "pwd"
+echo "[pwd]"
 pwd
 rm -rf public
+echo "[list all]"
 ls -al
